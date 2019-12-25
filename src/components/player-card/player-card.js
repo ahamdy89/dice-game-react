d0dd07bc5760eb3fd1990b89f0f45000a57a7957
ahamdy89 <ahmedhamdy_89@hotmail.com>
2019-12-25 16:25:28 +0200

    creating add player form and make it like a player card instead of a button

--- src/components/player-card/player-card.js
+++ src/components/player-card/player-card.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 import './player-card.scss';
 
 class PlayerCard extends Component {
     render() {
-        const {player, number} = this.props;
+        const {name, number, add, click} = this.props;
         return (
-            <div className="player-card-container">
-                <div className="player-number">
-                    <p>Player 1</p>
+            <div className="player-card-container" onClick={click}>
+                {add ?
+                <p>Add Player <FontAwesomeIcon icon={faUserPlus}/></p>
+                :
+                <div>
+                    <div className="player-number">
+                        <p>{`Player ${number + 1}`}</p>
+                    </div>
+                    <div className="player-name">
+                        <p>{name}</p>
+                    </div>
                 </div>
-                <div className="player-name">
-                    <p>Ahmed Hamdy</p>
-                </div>
+                }
             </div>
         );
     }
--- src/containers/game-layout/game-layout.js
+++ src/containers/game-layout/game-layout.js
@@ -1,58 +1,27 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import './game-layout.scss';
-// import AddForm from '../../components/add-form/form';
-import { addPlayers } from '../../redux/actions/players';
-import {Row, Col, Modal, Button } from 'antd';
 import PlayersLayout from '../players-layout/players-layout';
 
 
 
 class GameLayout extends Component {
-    state = {
-        modal : false,
-    }
-
-    toggle = () => {
-        this.setState({
-            modal : !this.state.modal,
-        })
-    }
-
-    handleSubmit = (values) => {
-        this.props.addPlayers(values);
-        this.setState({modal: false})
-
-    }
-
-    componentDidMount () {
-        const players = this.props.newPlayers;
-        console.log(players)
-    }
 
 
     render() {
         const players = this.props.newPlayers;
         console.log(players)
         return (
-                        <div className="game-layout-container">
-                                    <div className="header-container">
-                                            <div className="add-player-button">
-                                                <Button type="primary" onClick={this.toggle}>Add New Player</Button>
-                                                <Modal
-                                                    title="Add new players"
-                                                    visible={this.state.modal}
-                                                    onCancel={this.toggle}
-                                                    footer={null}
-                                                >
-                                                    {/* <AddForm submit={this.handleSubmit}/> */}
-                                                </Modal>
-                                            </div>
-                                    </div>
-                                    <div className="players-container">
-                                        <PlayersLayout players={players}/>
-                                    </div>
-                        </div>
+                <div className="game-layout-container">
+                    <div className="header-container">
+                            <div className="add-player-button">
+
+                            </div>
+                    </div>
+                    <div className="players-container">
+                        <PlayersLayout players={players}/>
+                    </div>
+                </div>
         );
     }
 }
@@ -62,4 +31,4 @@ class GameLayout extends Component {
 const mapStateToProps = state => ({
     newPlayers: state.playersList.players.players
 })
-export default connect(mapStateToProps,{addPlayers})(GameLayout);
+export default connect(mapStateToProps)(GameLayout);
